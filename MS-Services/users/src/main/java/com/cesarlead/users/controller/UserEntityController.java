47939ed1961cce65a5c3d22710414dd77f11d8e9
CesarLead <services@cesarlead.com>
2025-05-11 13:19:40 -0400

    Feat: "Se agrega swagger para doc en ms-user"

--- MS-Services/users/src/main/java/com/cesarlead/users/controller/UserEntityController.java
+++ MS-Services/users/src/main/java/com/cesarlead/users/controller/UserEntityController.java
@@ -4,6 +4,8 @@ import com.cesarlead.users.model.dtos.UserRequestDto;
 import com.cesarlead.users.model.dtos.UserResponseDto;
 import com.cesarlead.users.model.exception.NotFoundException;
 import com.cesarlead.users.service.UserEntityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,18 +23,21 @@ import java.util.UUID;
 @Validated
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Users", description = "Users API")
 public class UserEntityController {
 
     private static final String MESSAGE = "Requesting user";
     private final UserEntityService service;
 
     @GetMapping
+    @Operation(summary = "Get all users")
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         log.info("Requesting all users");
         return ResponseEntity.ok(service.getAllUsers());
     }
 
     @GetMapping("/username/{username}")
+    @Operation(summary = "Get user by username")
     public ResponseEntity<UserResponseDto> getUserByUsername(@PathVariable String username) {
         log.info(MESSAGE + " {}", username);
         return ResponseEntity.ok(service.getUserByUsername(username)
@@ -40,6 +45,7 @@ public class UserEntityController {
     }
 
     @GetMapping("/email/{email}")
+    @Operation(summary = "Get user by email")
     public ResponseEntity<UserResponseDto> getUserByEmail(@PathVariable String email) {
         log.info(MESSAGE + " {}", email);
         return ResponseEntity.ok(service.getUserByEmail(email)
@@ -47,6 +53,7 @@ public class UserEntityController {
     }
 
     @GetMapping("/id/{id}")
+    @Operation(summary = "Get user by id")
     public ResponseEntity<UserResponseDto> getUserById(@PathVariable UUID id) {
         log.info(MESSAGE + " {}", id);
         return ResponseEntity.ok(service.findById(id)
@@ -54,18 +61,21 @@ public class UserEntityController {
     }
 
     @PostMapping
+    @Operation(summary = "Create user")
     public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserRequestDto user) {
         log.info("Creating user {}", user.email());
         return ResponseEntity.created(URI.create("/" + service.createUser(user).username())).build();
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update user")
     public ResponseEntity<UserResponseDto> updateUser(@Valid @PathVariable UUID id, @Valid @RequestBody UserResponseDto user) {
         log.info("Updating user {}", id);
         return ResponseEntity.ok(service.updateUser(id, user));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete user")
     public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
         log.info("Deleting user {}", id);
         service.deleteUser(id);
